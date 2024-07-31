5f22e85451dcdc51d5f1be856a4b1c7a5e49b531
edvardsanta <eduardosantamarco123@outlook.com>
2024-07-31 16:34:43 -0300

    Modified config
    - Crete structs to manage different configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,74 +6,90 @@ import (
 	"github.com/spf13/viper"
 )
 
-var BotToken string
-var NewsChannelID string
-var OlympicChannelID string
-var OlympicChannelFinishedID string
-var OlympicChannelRunningID string
-var GuildID string
-var RedisAddr string
+type BotConfig struct {
+	Token                    string `mapstructure:"bot_token"`
+	NewsChannelID            string `mapstructure:"news_channel_id"`
+	OlympicChannelID         string `mapstructure:"olympic_channel_id"`
+	OlympicChannelFinishedID string `mapstructure:"olympic_channel_finished_id"`
+	OlympicChannelRunningID  string `mapstructure:"olympic_channel_running_id"`
+	GuildID                  string `mapstructure:"guild_id"`
+}
+
+type RedisConfig struct {
+	Addr string `mapstructure:"redis_addr"`
+}
+
+type PostgresConfig struct {
+	Host     string `mapstructure:"postgres_host"`
+	Port     string `mapstructure:"postgres_port"`
+	User     string `mapstructure:"postgres_user"`
+	Password string `mapstructure:"postgres_password"`
+	DBName   string `mapstructure:"postgres_db_name"`
+}
+
+type Config struct {
+	Bot         BotConfig      `mapstructure:"bot"`
+	Redis       RedisConfig    `mapstructure:"redis"`
+	Postgres    PostgresConfig `mapstructure:"postgres"`
+	Environment string         `mapstructure:"environment"`
+}
+
+var AppConfig Config
 
 func LoadConfig() {
-	// Configurar Viper para ler variáveis de ambiente
 	viper.AutomaticEnv()
-	viper.SetEnvPrefix("app") // Prefixo para variáveis de ambiente, exemplo: APP_BOT_TOKEN
+	viper.SetEnvPrefix("APP")
 
+	// Carregar configurações de acordo com o ambiente
 	environment := viper.GetString("ENVIRONMENT")
+	log.Printf("ENVIRONMENT: %s", environment)
 	if environment == "production" {
-		// Apenas variáveis de ambiente em produção
 		loadFromEnv()
 	} else {
-		// Carregar de arquivo e variáveis de ambiente em desenvolvimento
 		loadFromFileAndEnv(".env", "env", ".")
 	}
+
+	var botConfig BotConfig
+	var redisConfig RedisConfig
+	var postgresConfig PostgresConfig
+
+	err := viper.Unmarshal(&botConfig)
+	if err != nil {
+		log.Fatalf("Não foi possível carregar a configuração do bot: %v", err)
+	}
+
+	err = viper.Unmarshal(&redisConfig)
+	if err != nil {
+		log.Fatalf("Não foi possível carregar a configuração do Redis: %v", err)
+	}
+
+	err = viper.Unmarshal(&postgresConfig)
+	if err != nil {
+		log.Fatalf("Não foi possível carregar a configuração do PostgreSQL: %v", err)
+	}
+	// Atribuir valores para a configuração global
+	AppConfig = Config{
+		Bot:      botConfig,
+		Redis:    redisConfig,
+		Postgres: postgresConfig,
+	}
+	validateConfig()
 }
 
 func loadFromEnv() {
-	viper.BindEnv("BOT_TOKEN")
-	viper.BindEnv("NEWS_CHANNEL_ID")
-	viper.BindEnv("OLYMPIC_CHANNEL_ID")
-	viper.BindEnv("OLYMPIC_CHANNEL_FINISHED_ID")
-	viper.BindEnv("OLYMPIC_CHANNEL_RUNNING_ID")
-	viper.BindEnv("GUILD_ID")
-	viper.BindEnv("REDIS_ADDR")
-	BotToken = viper.GetString("BOT_TOKEN")
-	if BotToken == "" {
-		log.Fatal("BOT_TOKEN não está definido como variável de ambiente")
+	envVars := []string{
+		"bot_token", "news_channel_id", "olympic_channel_id",
+		"olympic_channel_finished_id", "olympic_channel_running_id",
+		"guild_id", "redis_addr", "postgres_host", "postgres_port",
+		"postgres_user", "postgres_password", "postgres_db_name",
 	}
 
-	NewsChannelID = viper.GetString("NEWS_CHANNEL_ID")
-	if NewsChannelID == "" {
-		log.Fatal("NEWS_CHANNEL_ID não está definido como variável de ambiente")
-	}
-
-	OlympicChannelID = viper.GetString("OLYMPIC_CHANNEL_ID")
-	if OlympicChannelID == "" {
-		log.Fatal("OLYMPIC_CHANNEL_ID não está definido como variável de ambiente")
-	}
-
-	OlympicChannelFinishedID = viper.GetString("OLYMPIC_CHANNEL_FINISHED_ID")
-	if OlympicChannelFinishedID == "" {
-		log.Fatal("OLYMPIC_CHANNEL_FINISHED_ID não está definido como variável de ambiente")
-	}
-
-	OlympicChannelRunningID = viper.GetString("OLYMPIC_CHANNEL_RUNNING_ID")
-	if OlympicChannelRunningID == "" {
-		log.Fatal("OLYMPIC_CHANNEL_RUNNING_ID não está definido como variável de ambiente")
-	}
-
-	GuildID = viper.GetString("GUILD_ID")
-	if GuildID == "" {
-		log.Fatal("GUILD_ID não está definido como variável de ambiente")
-	}
-
-	RedisAddr = viper.GetString("REDIS_ADDR")
-	if RedisAddr == "" {
-		log.Fatal("REDIS_ADDR não está definido como variável de ambiente")
+	for _, envVar := range envVars {
+		viper.BindEnv(envVar)
 	}
 }
 
-func loadFromFileAndEnv(configName string, configType string, configPath string) {
+func loadFromFileAndEnv(configName, configType, configPath string) {
 	viper.SetConfigName(configName)
 	viper.SetConfigType(configType)
 	viper.AddConfigPath(configPath)
@@ -81,7 +97,32 @@ func loadFromFileAndEnv(configName string, configType string, configPath string)
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Printf("Erro ao carregar o arquivo de configuração: %v", err)
+	} else {
+		log.Printf("Arquivo de configuração %s carregado com sucesso", configName)
 	}
 
 	loadFromEnv()
+}
+
+func validateConfig() {
+	requiredFields := map[string]string{
+		"bot_token":                   AppConfig.Bot.Token,
+		"news_channel_id":             AppConfig.Bot.NewsChannelID,
+		"olympic_channel_id":          AppConfig.Bot.OlympicChannelID,
+		"olympic_channel_finished_id": AppConfig.Bot.OlympicChannelFinishedID,
+		"olympic_channel_running_id":  AppConfig.Bot.OlympicChannelRunningID,
+		"guild_id":                    AppConfig.Bot.GuildID,
+		"redis_addr":                  AppConfig.Redis.Addr,
+		// "postgres_host":               AppConfig.Postgres.Host,
+		// "postgres_port":               AppConfig.Postgres.Port,
+		// "postgres_user":               AppConfig.Postgres.User,
+		// "postgres_password":           AppConfig.Postgres.Password,
+		// "postgres_db_name":            AppConfig.Postgres.DBName,
+	}
+
+	for key, value := range requiredFields {
+		if value == "" {
+			log.Fatalf("%s não está definido", key)
+		}
+	}
 }
