a1753fef587df9f557be1a60c19daede12b66092
edvardsanta <eduardosantamarco123@outlook.com>
2025-08-20 19:03:15 -0300

    feat: implement event dispatcher for handling messages with graceful shutdown

--- dokja_lab/main.py
+++ dokja_lab/main.py
@@ -1,26 +1,60 @@
-from config import SCRAPERS, STORAGE
+import signal
+import sys
+
+from config import HANDLER_FACTORY
 from logging_config import get_logger
+import threading
+from infra.subscriber import BaseSubscriber
 
 logger = get_logger(__name__)
+stop_event = threading.Event()
 
-import time
-from memes.pool import MemePool
+def event_dispatcher(subscriber: BaseSubscriber):
+    try:
+        while not stop_event.is_set():
+            message = subscriber.receive_item() # Simulated message for testing
+            if not message:
+                continue
+
+            prefix, _, data = message.partition(":")
+            handlers = HANDLER_FACTORY.get_handlers(prefix)
+
+            if not handlers:
+                logger.warning("No handlers registered for event type '%s'", prefix)
+                continue
+
+            for handler in handlers:
+                try:
+                    handler(data)
+                except Exception as e:
+                    logger.exception(
+                        "Error in handler %s for event '%s'",
+                        getattr(handler, "__class__", type(handler)).__name__,
+                        prefix,
+                    )
+    except Exception as e:
+        logger.exception("Fatal error in dispatcher loop: %s", e)
+    finally:
+        subscriber.close()
+
 
 def main():
-    pool = MemePool(scrapers=SCRAPERS, storage=STORAGE)
 
-    while True:
-        logger.info("Refreshing meme pool...")
-        pool.refresh_pool()
-        logger.info("Meme pool refreshed")
+    event_thread = threading.Thread(
+        target=event_dispatcher, args=(None,), daemon=True
+    )
 
-        meme = pool.get_next_meme()
-        if meme:
-            logger.info(f"Next meme: {meme['title']} ({meme['url']})")
-        else:
-            logger.warning("No memes available in pool")
+    def shutdown(signum, frame):
+        logger.info("Shutting down dispatcher...")
+        stop_event.set()
+        event_thread.join()
+        sys.exit(0)
 
-        time.sleep(60)
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
+
+    event_thread.start()
+    event_thread.join()
 
 
 if __name__ == "__main__":
