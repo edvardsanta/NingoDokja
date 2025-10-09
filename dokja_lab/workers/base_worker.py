166857d9855d27ab5c95b56ff96e31ad443e7da5
edvardsanta <eduardosantamarco123@outlook.com>
2025-10-09 20:35:55 -0300

    fix(base_worker): specify return type for run method

--- dokja_lab/workers/base_worker.py
+++ dokja_lab/workers/base_worker.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class BaseWorker(ABC):
     @abstractmethod
-    def run(self):
+    def run(self) -> Any:
         """Execute the worker task"""
         pass
